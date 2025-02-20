b423f4a8bb7ce569f40ba0746f443e4d25de853f
theseekerrr <priyaseekerrr@gmail.com>
2025-02-20 10:17:42 +0530

    check leap year or not

---
+++ leapYear.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class leapYear {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter a year to check leap year:");
+        int year=sc.nextInt();
+        if((year%4==0 && year%100!=0)||(year%400==0)){
+            System.out.println(year+" it is leap year: ");
+        }
+        else{
+            System.out.println(year+" it is not leap year");
+        }
+        sc.close();
+    }
+}
