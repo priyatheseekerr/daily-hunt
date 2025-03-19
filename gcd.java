93efd1c029d0456111ef3c6630402d33ea7bf067
theseekerrr <priyaseekerrr@gmail.com>
2025-03-19 16:46:59 +0530

    program to find gcd/hcf of two numbers

---
+++ gcd.java
@@ -0,0 +1,21 @@
+//program to find gcd/hcf of two numbers
+import java.util.Scanner;
+
+public class gcd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter two number to find lcm: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int a1 = a;
+        int b1 = b;
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+       }
+       System.out.println(a);
+       sc.close();
+    }
+    
+}
